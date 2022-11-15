231e4460c56426e66c62e77e96634d1786f840be
yykoca <yykoca.08@gmail.com>
2022-11-15 23:15:09 +0100

    logger&twig interface

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,12 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
 use function Symfony\component\string\u;
 
 class VinylController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    function homepage(): Response
+    function homepage(Environment $twig): Response
     {
         $tracks = [
             ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
@@ -25,6 +27,12 @@ class VinylController extends AbstractController
         // dd($tracks);
         // dump($tracks);
         
+        // $html = $twig->render('vinyl/homepage.html.twig', [
+        //     'title' => 'PB & Jams',
+        //     'tracks' => $tracks,
+        // ]);
+        // return new Response($html);
+
         return $this->render('vinyl/homepage.html.twig', [
             'title' => 'PB & Jams',
             'tracks' => $tracks,
